7ad3ba243ae1ee6f5bfc09dcaea3b840f5be452f
diogossoares <diogo.ssantos30@gmail.com>
2020-04-30 20:14:52 -0300

    3 commit

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
+function addCard(){
+    document
+        .querySelector("#modal")
+        .classlist
+        .toggle("hide")
 
 
-function on0ff(){
-document
-    .querySelector("#modal")
-    .classlist
-    .toggle("hide")
 
+console.log(toggle);
+
+    document
+    .querySelector("body")
+    .classList
+    .toggle("hideScroll")
+
+    document
+    .querySelector("modal")
+    .classList
+    .toggle("addScroll")
 }
-
