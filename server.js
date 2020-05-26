e5efad31e77a77ba09d75acd4029af13244092a4
diogossoares <diogo.ssantos30@gmail.com>
2020-05-25 22:24:48 -0300

    25/maio/2020

--- server.js
+++ server.js
@@ -7,30 +7,44 @@ const ideas = [
         img:"https://image.flaticon.com/icons/svg/2729/2729007.svg",
         title:"Curso de programação",
         category:"Estudo",
-        Description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
+        description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
         url:"https://rocketseat.com.br"
     },
     {
         img:"https://image.flaticon.com/icons/svg/2729/2729005.svg",
         title:"Exercícios",
         category:"Saúde",
-        Description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
+        description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
         url:"https://rocketseat.com.br"
     },
     {
         img:"https://image.flaticon.com/icons/svg/2729/2729027.svg",
         title:"Meditação",
         category:"Mentalidade",
-        Description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
+        description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
         url:"https://rocketseat.com.br"
     },
     {
         img:"https://image.flaticon.com/icons/svg/2729/2729032.svg",
         title:"Karaokê",
         category:"Diversão em Família",
-        Description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
+        description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
         url:"https://rocketseat.com.br"
-    }
+    },
+    {
+        img:"https://image.flaticon.com/icons/svg/2729/2729038.svg",
+        title:"Pintura",
+        category:"Criatividade",
+        description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
+        url:"https://rocketseat.com.br"
+    },
+    {
+        img:"https://image.flaticon.com/icons/svg/2729/2729048.svg",
+        title:"Recortes",
+        category:"Criatividade",
+        description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid rem, facilis asperiores dolorum reiciendis voluptates illum totam,",
+        url:"https://rocketseat.com.br"
+    },
 ]
 
 
@@ -50,11 +64,21 @@ nunjucks.configure("views", {
 
 //criei uma rotina/
 //e capturo o pedido do cliente para responder
-server.get("/",function(req, res) {
-    return res.render("index.html", {ideas})
+server.get("/", function(req, res) {
+    const reversedIdeas = [...ideas].reverse()
+    let lastIdeas = []
+    for (let idea of reversedIdeas) {
+        if(lastIdeas.length < 2) {
+            lastIdeas.push(idea)
+        }
+    }
+
+    return res.render("index.html", {ideas: lastIdeas})
 })
 server.get("/ideias",function(req, res) {
-    return res.render( "ideias.html")
+    const reversedIdeas = [...ideas].reverse()
+
+    return res.render( "ideias.html", {ideas:reversedIdeas})
 })
 //liguei meu servidor na porta 3000
-server.listen(3003)
+server.listen(3000)
