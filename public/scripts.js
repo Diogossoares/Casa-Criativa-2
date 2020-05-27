d256924a17593352f41faf65313628ba2d2c3803
diogossoares <diogo.ssantos30@gmail.com>
2020-05-27 00:10:10 -0300

    fim do casa criativa

--- public/scripts.js
+++ public/scripts.js
@@ -18,3 +18,28 @@ console.log(toggle);
     .classList
     .toggle("addScroll")
 }
+
+function checkFields(event){
+    const valuesToCheck = [
+        "title",
+        "image",
+        "category",
+        "description",
+        "link",
+    ]
+
+    const isEmpty = valuesToCheck.find(function(value){
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIfIsString = ! event.target[value].value.trim()
+        if(checkIfIsString && checkIfIsEmpty){
+            return true
+        }
+    })
+
+
+        if(isEmpty){
+            event.preventDefault()
+            alert("por favor, preencha todos os campos")
+        }
+    
+}
